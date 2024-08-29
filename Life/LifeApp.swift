21b80b36b43c61db322aa1c8de735aed22053833
“Victor <75052396+the1percentSFX@users.noreply.github.com>
2024-08-28 23:00:52 -0500

    Initial Commit

---
+++ Life/LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeApp.swift
+//  Life
+//
+//  Created by Victor Ramirez on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
